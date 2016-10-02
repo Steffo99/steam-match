a196a6457e381e40c3b9d7b14dba8670319e0a2f
Stefano Pigozzi <ste.pigozzi@gmail.com>
2016-10-02 19:56:25 +0200

    First (and probably last) commit

---
+++ steammatch.py
@@ -0,0 +1,35 @@
+import requests
+
+# Get the Steam API key from steamkey.txt
+f = open("steamkey.txt", "r")
+key = f.read()
+f.close()
+del f
+
+
+def get_steam_games_owned(steamid: int) -> list:
+    # Request the list of games played
+    params = {
+        "steamid": steamid,
+        "key": key,
+        "include_played_free_games": True,
+        "format": "json",
+        "include_appinfo": 1
+    }
+    r = requests.get("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/", params=params)
+    if r.status_code != 200:
+        raise Exception("Error during the API request.")
+    # Extract app ids
+    games_owned = list()
+    data = r.json()
+    for game in data["response"]["games"]:
+        games_owned.append(game["name"])
+    # Return the list
+    return games_owned
+
+
+def compare(steamids: list):
+    current = set(get_steam_games_owned(steamids.pop(0)))
+    for player in steamids:
+        current = current & set(get_steam_games_owned(player))
+    return current

5101cfc28fd2d65eb142c08b44aa7805a12c66f1
Stefano Pigozzi <ste.pigozzi@gmail.com>
2016-12-21 18:49:27 +0100

    New features

--- steammatch.py
+++ steammatch.py
@@ -7,6 +7,18 @@ f.close()
 del f
 
 
+def resolve_vanity(name: str) -> int:
+    r = requests.get("http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key={key}&vanityurl={name}".format(key=key, name=name))
+    if r.status_code != 200:
+        raise Exception("Error during the API request.")
+    j = r.json()["response"]
+    if j["success"] == 42:
+        raise Exception("Vanity url not found")
+    elif j["success"] != 1:
+        raise Exception("Unknown exception: {message}".format(j["message"]))
+    return j["steamid"]
+
+
 def get_steam_games_owned(steamid: int, freetoplay=True) -> list:
     # Request the list of games played
     params = {
@@ -30,6 +42,11 @@ def get_steam_games_owned(steamid: int, freetoplay=True) -> list:
 
 
 def and_games(steamids: list):
+    for index, player in enumerate(steamids):
+        try:
+            steamids[index] = int(player)
+        except ValueError:
+            steamids[index] = int(resolve_vanity(player))
     current = set(get_steam_games_owned(steamids.pop(0)))
     for player in steamids:
         current = current & set(get_steam_games_owned(player))
@@ -37,6 +54,11 @@ def and_games(steamids: list):
 
 
 def or_games(steamids: list):
+    for index, player in enumerate(steamids):
+        try:
+            steamids[index] = int(player)
+        except ValueError:
+            steamids[index] = int(resolve_vanity(player))
     current = set(get_steam_games_owned(steamids.pop(0)))
     for player in steamids:
         current = current | set(get_steam_games_owned(player))
@@ -44,10 +66,20 @@ def or_games(steamids: list):
 
 
 def diff_games(first, second):
+    for index, player in enumerate(steamids):
+        try:
+            steamids[index] = int(player)
+        except ValueError:
+            steamids[index] = int(resolve_vanity(player))
     current = set(get_steam_games_owned(first, False)) - set(get_steam_games_owned(second, False))
     return current
 
 
 def xor_games(first, second):
+    for index, player in enumerate(steamids):
+        try:
+            steamids[index] = int(player)
+        except ValueError:
+            steamids[index] = int(resolve_vanity(player))
     current = set(get_steam_games_owned(first)) ^ set(get_steam_games_owned(second))
     return current
